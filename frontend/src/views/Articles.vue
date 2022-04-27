fac9ef2df704457a075b47b12e8132528e9a9535
Brice THIBAULT <bricethibault613@gmail.com>
2022-04-27 08:53:23 +0200

    Update Articles.vue

--- frontend/src/views/Articles.vue
+++ frontend/src/views/Articles.vue
@@ -3,28 +3,26 @@
     <div>
       <h1 class="card-titlepost">Visualisation de tous les articles !!!</h1>
     </div>
-    <li 
-    :class="{active: index == currentIndex}"
-    v-for="(article,index) in articles"
-    :key="index" 
-    class="allArticles">
-    {{article}}
-    </li>
+    <p
+      v-for="article in articles" :key="article"
+    >
+      <!-- {{ article[0].title }} A garder car Ok sauf que je veux que tout s'affiche-->
+      {{ article }}
+    </p>
   </div>
-
 </template>
 
 <script>
 export default {
-  name: 'allArticles',
-  data(){
-    return{
+  name: "allArticles",
+  data() {
+    return {
       userId: "",
       isAdmin: "",
-      articles: []
-    }
+      articles: [],
+    };
   },
-  mounted(){
+  mounted() {
     this.userId = JSON.parse(localStorage.getItem("userId"));
     this.isAdmin = JSON.parse(localStorage.getItem("isAdmin"));
     console.log(localStorage);
@@ -33,18 +31,18 @@ export default {
     let options = {
       method: "GET",
       headers: {
-        "Authorization": "Bearer " + localStorage.getItem("token"),
+        Authorization: "Bearer " + localStorage.getItem("token"),
       },
     };
-    console.log(options)
+    console.log(options);
     fetch(urlAllArticles, options)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data, "ligne44");
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
         this.articles = data;
         console.log(this.articles);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   },
-}
+};
 </script>

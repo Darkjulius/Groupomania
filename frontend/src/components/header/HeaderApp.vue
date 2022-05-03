65a1fbaa682ae2a71e7419ab2db04e6618b1f26e
Brice THIBAULT <bricethibault613@gmail.com>
2022-05-03 08:04:26 +0200

    Update HeaderApp.vue
    Ajouter des router-link pour les pages:
    Accueil, Ajout Article, Profil et Déconnexion

--- frontend/src/components/header/HeaderApp.vue
+++ frontend/src/components/header/HeaderApp.vue
@@ -2,8 +2,10 @@
     <header>
         <img src="../../assets/icon-left-font-monochrome-white.png" width="240" height="240" alt="logo gropomania" title="logo groupomania" class="img-fluid" />
         <nav class="navbarre">
-            <router-link to="/home" title="Liste des articles"> <button @click="disconnect()" title="Déconnexion">Déconnexion</button></router-link>
-            <router-link to="/add" title="Ajouter un article"><span class="ajout-article">Ajout Article</span></router-link>
+            <router-link to="/home"><span class="nav-barre">Accueil</span></router-link>
+            <router-link to="/add"><span class="nav-barre">Ajout Article</span></router-link>
+            <router-link to="/account"><span class="nav-barre">Profil</span></router-link>
+            <button @click="disconnect" class="disconnect">Déconnexion</button>
         </nav>
     </header>
 </template>
@@ -26,7 +28,7 @@ header{
     height: 10vh;
     background-color: #122442;
 }
-.ajout-article{
+.nav-barre{
     color: #fff;
     text-decoration: none;
     
